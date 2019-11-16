6100eebe76b671dec46718a65ba4dafabf5e421d
Jeremy Rogel <jeremyrogel@Jeremys-MBP.lan1>
2019-11-16 13:14:45 -0500

    asdf

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
     client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
-      filename: './data/files/auth.db3',
+      filename: './data/users.db3',
     },
     pool: {
       afterCreate: (conn, done) => {
@@ -23,7 +23,7 @@ module.exports = {
     client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
-      filename: './data/files/auth.db3',
+      filename: './data/users.db3',
     },
     pool: {
       afterCreate: (conn, done) => {
