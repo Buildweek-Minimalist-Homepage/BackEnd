fbcb7dbd5fbdf368041c74bfd6a665a3ebe98218
Jnmendza <jonatmendoza@gmail.com>
2019-11-17 13:20:04 -0800

    first test running and passing

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,21 +1,26 @@
 const request = require('supertest');
 const server = require('./server');
 
-describe('GET /', () => {
-    it('Should return 200', () => {
-        return request(server).get('/').then(res => {
-            expect(res.status).toBe(200);
-        });
-    });
+// describe('GET /', () => {
+//     it('Should return 200', () => {
+//         return request(server).get('/').then(res => {
+//             expect(res.status).toBe(200);
+//         });
+//     });
 
-    it('Should return JSON', async () => {
-        const response = await request(server).get('/');
-        expect(response.type).toMatch(/json/i);
-    });
+//     it('Should return JSON', async () => {
+//         const response = await request(server).get('/');
+//         expect(response.type).toMatch(/json/i);
+//     });
 
-    it('Should return Frankenstein quote', () => {
-        return request(server).get('/').then(res => {
-            expect(res.body).toEqual({ message: "It's aliveeeee!" });
-        });
-    });
+//     it('Should return Frankenstein quote', () => {
+//         return request(server).get('/').then(res => {
+//             expect(res.body).toEqual({ message: "It's aliveeeee!" });
+//         });
+//     });
+// })
+
+it('should set db ENV to testing', function() {
+    expect(process.env.DB_ENV)
+    .toBe('testing')
 })
