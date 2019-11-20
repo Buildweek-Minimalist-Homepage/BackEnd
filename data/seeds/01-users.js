59a192c3de6ca6b8d1e22d810b9fa306892dd35e
Jnmendza <jonatmendoza@gmail.com>
2019-11-20 10:10:27 -0800

    updated seed file

--- data/seeds/01-users.js
+++ data/seeds/01-users.js
@@ -5,5 +5,6 @@ exports.seed = function(knex) {
     return knex ('users').insert([
         { email: 'test01@test.com', name: 'test1', password: bcrypt.hashSync("password", 10) },
         { email: 'test02@test.com', name: 'test2', password: bcrypt.hashSync("password", 10) },
+        { email: 'test03@test.com', name: 'test3', password: bcrypt.hashSync("password", 10) },
     ])
 }
