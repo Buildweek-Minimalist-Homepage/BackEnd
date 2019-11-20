5ed18c64f4fa0562f8518d778049cfc971b7210d
Jnmendza <jonatmendoza@gmail.com>
2019-11-20 11:00:28 -0800

    updated quotes API

--- quotes/quotes-router.js
+++ quotes/quotes-router.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const fetch = require('node-fetch');
 
 router.get('/', async (req, res) => {
-    const api_url = `https://api.paperquotes.com/apiv1/quotes/?tags=love,life&curated=1`
+    const api_url = `https://api.quotable.io/random`
     const response = await fetch(api_url);
     const json = await response.json();
     res.json(json);
